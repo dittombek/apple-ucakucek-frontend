5f92d541d907215752de8d38138e8c6ce63c154d
Ditto <dittodoangyak@gmail.com>
2026-05-20 07:59:42 +0700

    Initial Commit

---
+++ Nutrizzah/NutrizzahApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrizzahApp.swift
+//  Nutrizzah
+//
+//  Created by Ryandra Anditto on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NutrizzahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
